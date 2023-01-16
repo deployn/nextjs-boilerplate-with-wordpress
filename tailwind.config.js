69f06f8858d766a909e25492b725c80b5dbf1262
Jewgeni Lewash <j.lewash@gmail.com>
2023-01-16 23:14:59 +0100

    feat(tailwind.config.js): extend tailwind config with primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#fef2f3',
+          100: '#fce7e8',
+          200: '#f9d2d7',
+          300: '#f4adb5',
+          400: '#ed7f8e',
+          500: '#e25169',
+          600: '#cd3152',
+          700: '#bb2649',
+          800: '#91203e',
+          900: '#7c1f3a',
+        },
+      },
+    },
   },
   plugins: [],
 };

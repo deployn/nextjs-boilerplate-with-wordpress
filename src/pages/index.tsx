988b4a0ded130866940a7437dc5e845c36e70a55
Jewgeni Lewash <j.lewash@gmail.com>
2023-01-24 12:12:31 +0100

    feat(index.tsx): add some styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,21 +31,30 @@ const Index: NextPage<IndexProps> = ({ allPosts: { edges } }) => {
       </h1>
       {heroPost && (
         <section>
-          <h2>Hero Post</h2>
-          <Link href={`/posts/${heroPost.slug}`}>
-            <h3>{heroPost.title}</h3>
+          <h2 className="mb-4 text-2xl font-medium">Hero Post</h2>
+          <Link className="mb-2 block" href={`/posts/${heroPost.slug}`}>
+            <h3 className="text-xl font-medium">{heroPost.title}</h3>
           </Link>
-          <div dangerouslySetInnerHTML={{ __html: heroPost.excerpt }} />
+          <div
+            className="mb-6"
+            dangerouslySetInnerHTML={{ __html: heroPost.excerpt }}
+          />
         </section>
       )}
       {morePosts.length > 0 && (
         <section>
-          <h2>More Posts</h2>
-          <div>
+          <h2 className="mb-4 text-2xl font-medium">More Posts</h2>
+          <ul className="list-none">
             {morePosts.map(({ node }) => (
-              <h3 key={node.slug}>{node.title}</h3>
+              <li key={node.slug} className="mb-4">
+                <Link className="block" href={`/posts/${node.slug}`}>
+                  <h3 className="text-xl font-medium" key={node.slug}>
+                    {node.title}
+                  </h3>
+                </Link>
+              </li>
             ))}
-          </div>
+          </ul>
         </section>
       )}
       <Link href="https://github.com/deployn/nextjs-boilerplate-with-wordpress">

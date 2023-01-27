d20e664b0a4acd1759ad76b77e9fbb54fc5206f1
Jewgeni Lewash <j.lewash@gmail.com>
2023-01-27 10:52:33 +0100

    feat(next.config.js): image optimisation for wordpress images

--- next.config.js
+++ next.config.js
@@ -14,4 +14,13 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
 });

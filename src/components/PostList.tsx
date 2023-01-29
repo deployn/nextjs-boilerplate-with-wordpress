dd5a1e33240de8e318fd951f073c0005f4c6abb9
Jewgeni Lewash <j.lewash@gmail.com>
2023-01-29 11:35:13 +0100

    feat(PostList): add a postlist component to fetch 10 posts with apollo

---
+++ src/components/PostList.tsx
@@ -0,0 +1,50 @@
+import { gql, useQuery } from '@apollo/client';
+import DOMPurify from 'isomorphic-dompurify';
+
+export const ALL_POSTS_QUERY = gql`
+  query allPosts {
+    posts {
+      edges {
+        node {
+          id
+          title
+          content
+          slug
+        }
+      }
+    }
+  }
+`;
+
+export const allPostsQueryVars = {
+  skip: 0,
+  first: 10,
+};
+
+const PostList = () => {
+  const { data, loading, error } = useQuery(ALL_POSTS_QUERY, {
+    variables: allPostsQueryVars,
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const allPosts = data?.posts;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <ul>
+      {allPosts.edges.map((post: any) => {
+        const sanitizedHTML = DOMPurify.sanitize(post.node.content);
+        return (
+          <li key={post.node.id}>
+            <h2>{post.node.title}</h2>
+            <p>{sanitizedHTML}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default PostList;

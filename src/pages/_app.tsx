6acc681f6bc7bc57634a2c8f77308fb301d5c10c
Jewgeni Lewash <j.lewash@gmail.com>
2023-01-29 11:34:30 +0100

    feat(_app): wrap into apolloclient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import '../styles/global.css';
 import '../styles/wordpress.css';
 
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+import { useApollo } from '@/lib/apolloClient';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
 export default MyApp;

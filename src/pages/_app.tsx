e49d3c90dcfbac6ef3bc95f111df79f3ec1cc3b5
Jewgeni Lewash <j.lewash@gmail.com>
2023-01-31 07:58:51 +0100

    feat: add impulse.dev for live editing of tailwind classes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+
 import '../styles/global.css';
 import '../styles/wordpress.css';
 
 import type { AppProps } from 'next/app';
+
+if (process.env.NODE_ENV === 'development') {
+  import('@impulse.dev/runtime').then((impulse) =>
+    impulse.run({
+      tailwindConfig: require('../../tailwind.config'),
+    })
+  );
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />

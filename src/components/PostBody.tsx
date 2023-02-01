11f2d5ce1185143b187830213b70f443df9c2f1a
Jewgeni Lewash <j.lewash@gmail.com>
2023-02-01 11:09:20 +0100

    chore(PostBody): try to transform <a> tags into link component
    doesn't work at the moment

--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,5 +1,7 @@
-import HTMLReactParser from 'html-react-parser';
-import DOMPurify from 'isomorphic-dompurify';
+/* eslint-disable no-param-reassign */
+import type { HTMLReactParserOptions } from 'html-react-parser';
+import HTMLReactParser, { Element } from 'html-react-parser';
+import Dompurify from 'isomorphic-dompurify';
 
 import styles from '@/styles/PostBody.module.css';
 
@@ -8,10 +10,20 @@ type PostBodyProps = {
 };
 
 export default function PostBody({ content }: PostBodyProps) {
+  const options: HTMLReactParserOptions = {
+    replace: (domNode) => {
+      if (domNode instanceof Element && domNode.attribs) {
+        if (domNode.name === 'a') {
+          domNode.attribs.class = 'text-blue-500 hover:underline';
+        }
+      }
+    },
+  };
+
   return (
     <div className="mx-auto max-w-2xl">
       <div className={styles.content}>
-        {HTMLReactParser(DOMPurify.sanitize(content))}
+        {HTMLReactParser(Dompurify.sanitize(content), options)}
       </div>
     </div>
   );

b16aff618d71e77a2ca8cb7d580ea077fcc4d472
Jewgeni Lewash <j.lewash@gmail.com>
2023-02-01 11:03:25 +0100

    feat(Link): treat link to wp domain as internal

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,6 +2,8 @@ import NextLink from 'next/link';
 
 import ExternalLink from '@/components/ExternalLink';
 
+const wpDomain = process.env.WORDPRESS_API_URL as string;
+
 const style = {
   base: 'text-primary-500 hover:text-primary-700 transition duration-150 ease-in-out cursor-pointer focus:underline focus:text-primary-700 focus:shadow-outline focus:shadow-outline-blue',
 };
@@ -14,7 +16,7 @@ type LinkProps = {
 
 const Link = ({ href, children, className, ...props }: LinkProps) => {
   const classes = [style.base, className].join(' ');
-  if (href.toString().startsWith('/')) {
+  if (href.toString().startsWith('/') || href.toString().startsWith(wpDomain)) {
     return (
       <NextLink href={href} className={classes} {...props}>
         {children}

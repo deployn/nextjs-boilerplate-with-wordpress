c443e6ba0ed9aab4410de3bfefb54dfb5bfe5174
Jewgeni Lewash <j.lewash@gmail.com>
2023-02-22 07:51:05 +0100

    test: update index page test

--- src/pages.test/index.test.tsx
+++ src/pages.test/index.test.tsx
@@ -17,4 +17,44 @@ describe('Index', () => {
     render(<Index allPosts={{ posts: [] }} />);
     expect(screen.getByText('Index')).toBeInTheDocument();
   });
+
+  it('renders more posts', async () => {
+    const morePosts = [
+      {
+        title: 'More Post 1',
+        slug: 'more-post-1',
+      },
+      {
+        title: 'More Post 2',
+        slug: 'more-post-2',
+      },
+    ];
+    render(<Index allPosts={{ posts: morePosts }} />);
+    morePosts.forEach((post) => {
+      expect(screen.getByText(post.title)).toBeInTheDocument();
+    });
+  });
+
+  it('renders cookies', async () => {
+    render(<Index allPosts={{ posts: [] }} />);
+    expect(screen.getByText('Cookies: not accepted')).toBeInTheDocument();
+  });
+
+  it('renders cookies accepted', async () => {
+    Object.defineProperty(window.document, 'cookie', {
+      writable: true,
+      value: 'acceptedCookies=true',
+    });
+    render(<Index allPosts={{ posts: [] }} />);
+    expect(screen.getByText('Cookies: accepted')).toBeInTheDocument();
+  });
+
+  it('renders cookies not accepted', async () => {
+    Object.defineProperty(window.document, 'cookie', {
+      writable: true,
+      value: 'acceptedCookies=false',
+    });
+    render(<Index allPosts={{ posts: [] }} />);
+    expect(screen.getByText('Cookies: not accepted')).toBeInTheDocument();
+  });
 });

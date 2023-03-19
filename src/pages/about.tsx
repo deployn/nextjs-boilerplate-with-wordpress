0b1c2c71162d8a278b61c023cf8f88d4d5d2d48f
Jewgeni Lewash <j.lewash@gmail.com>
2023-03-19 20:39:56 +0100

    refactor: seperate cookie functions from page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,21 +1,10 @@
 import { useEffect, useState } from 'react';
 
 import Layout from '@/components/Layout';
+import { getCookie, setCookie } from '@/lib/cookies';
 
 export default function About() {
   const [consent, setConsent] = useState(false);
-
-  const getCookie = (name: any) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-      const cookie = parts.pop();
-      if (cookie) {
-        return cookie.split(';').shift();
-      }
-    }
-    return null;
-  };
 
   useEffect(() => {
     const acceptedCookie = getCookie('youtubeConsent');
@@ -25,13 +14,6 @@ export default function About() {
       setConsent(false);
     }
   }, [consent]);
-
-  const setCookie = (name: string, value: any, days: number) => {
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    const expires = `expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${value};${expires};path=/`;
-  };
 
   return (
     <Layout>
